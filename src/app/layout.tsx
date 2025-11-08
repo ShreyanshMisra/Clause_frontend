db70673a90329b9d87be0cc16a0629b1e9ef434a
SonnyZhan <fumingzhang@umass.edu>
2025-11-08 14:43:55 -0500

    highlighting works now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body suppressHydrationWarning>
         <Providers>
           <NextTopLoader color="#FF9578" showSpinner={false} />
 
